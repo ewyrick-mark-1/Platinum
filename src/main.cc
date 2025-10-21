1c18aa983657efff2b2ed9d85db7ae530c702ece
Tsuchu <abilbro005@gmail.com>
2025-10-21 14:19:36 -0400

    Testing SFML

---
+++ src/main.cc
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+int main(int argc, char** argv) {
+    sf::RenderWindow window (sf::VideoMode({800, 600}), "Platinum");
+    
+    while(window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+    }
+
+    return 0;
+}
